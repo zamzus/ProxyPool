fb84592738d6dfd22844dccc275ca1ef61813799
zamzus <260427977@qq.com>
2023-04-10 16:29:20 +0800

    Style(all): all

--- proxypool/settings.py
+++ proxypool/settings.py
@@ -1,8 +1,8 @@
-import platform
+import platforms
 from myserver import MAPPING_REDIS_CONFIG, KEY_PROXIES
 
 # definition of flags
-IS_WINDOWS = platform.system().lower() == 'windows'
+IS_WINDOWS = platforms.system().lower() == 'windows'
 
 # redis configure
 REDIS_CONFIG = MAPPING_REDIS_CONFIG['NDDC'].copy()
